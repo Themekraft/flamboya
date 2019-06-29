1c46f055f122114103970911b5f382d5db1ae00e
Catterine <katrin@zeitschel-design.de>
2019-06-29 11:15:00 +0200

    include js - scroll effect

---
+++ includes/js/windowscrollnav.js
@@ -0,0 +1,37 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (screen.width <= 600) { 
+  	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topnav").style.position = "fixed";
+    document.getElementById("topnav").style.background = "white";
+    document.getElementById("topnav").style.top = "0px";
+	} else {
+	    document.getElementById("topnav").style.position = "absolute";
+	    document.getElementById("topnav").style.background = "var(--flamboya-white)";
+	    document.getElementById("topnav").style.top = "auto";
+	  }
+}
+else
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topnav").style.height = "70px";
+    document.getElementById("topnav").style.position = "fixed";
+    document.getElementById("topnav").style.background = "white";
+    document.getElementById("topnav-logo").style.height = "130%";
+    document.getElementById("main-menu").style.padding = "10px 0px";
+
+	} else {
+	    document.getElementById("topnav").style.position = "absolute";
+	    document.getElementById("topnav").style.background = "var(--flamboya-white)";
+	    document.getElementById("topnav").style.height = "148px";
+	    document.getElementById("topnav-logo").style.height = "148px";
+	    document.getElementById("main-menu").style.padding = "81px 0px";
+	  }
+} 
+
+
+// $(document).ready(function() {
+// 	$(window).scroll(function(){
+// 		$("#tallContainer").css("background-color","black");
+// 	});
+// });
