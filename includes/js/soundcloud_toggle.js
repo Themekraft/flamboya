823998ef880a08f94dcb58024ff61d4598a804ee
svenl77 <mail@sven-lehnert.de>
2019-11-16 16:06:06 +0000

    js

--- includes/js/soundcloud_toggle.js
+++ includes/js/soundcloud_toggle.js
@@ -1,10 +1,7 @@
-$(function(){
-	$('#content').addClass("slideOutLeft");
-	
-	
-if($('.fap-wrapper').hasClass("fap-open")){
-	$('#content').animate({left:-200 + "px"}, 2000);
-	$('#content').addClass("slideOutLeft");
+function flamboya_add_class(){
+	if(jQuery( "#theflamboyaplayerid" ).hasClass( "fap-open" )){
+		jQuery('.container').addClass('flamboya_player_open');
+	} else {
+		jQuery('.container').removeClass('flamboya_player_open');
+	}
 }
-
-});
